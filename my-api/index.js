ee21ba4c7f0f628093460adf935f0201c74faba7
VietnameseDelegation <161819565+VietnameseDelegation@users.noreply.github.com>
2026-02-23 12:39:45 +0100

    Update index.js

--- my-api/index.js
+++ my-api/index.js
@@ -25,7 +25,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Catch-all route to serve the frontend (for client-side routing)
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
